f7374be1e40241b98bfd6d0f0ce5918fbda7efe1
abhishektps4 <abhishek1720044@gmail.com>
2024-12-08 17:16:26 +0000

    conversion of datatypes

--- 02_datatype.js
+++ 02_datatype.js
@@ -3,6 +3,23 @@
 let name = "abhishek"
 let age = 34
 let isLogged = false;
-console.table([name, age, isLogged])
-console.log(typeof "123")
-console.log(typeof null)
+//console.table([name, age, isLogged])
+//console.log(typeof "123")
+//console.log(typeof null)
+let score = "abhishek"
+//console.log(typeof score)
+/*
+let valueInNumber = Number(score)
+console.log(typeof score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+*/
+
+//"33" => 33
+// "33abc" => NaN
+//true => 1; false =>0;
+// 1 => true; 0 => false;
+// "abhishek" => true; "" => false;
+
+
+
