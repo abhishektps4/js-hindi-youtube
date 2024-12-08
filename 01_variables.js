b4adcf175ff557ddb396a56acd6d9c7af0f6c6d1
abhishektps4 <abhishek1720044@gmail.com>
2024-12-08 16:41:36 +0000

    variables, datatypes

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12344;
+let accountEmail = "abhishek@gmail.com";
+var accountPassword = "12345";
+accountCity = "Lucknow";
+
+//accountId = 2; // not allowed
+/*
+comment lines
+*/
+let accountState;
+
+accountEmail = "adt@gmail.com";
+accountPassword = "3422";
+accountCity = "sidhaul";
+console.table([accountEmail,accountId,accountPassword,accountCity , accountState]);
+
---
+++ 02_datatype.js
@@ -0,0 +1,8 @@
+"use strict"; // treate all JS code as newer version
+//alert('hello');//we are using nodejs not browser
+let name = "abhishek"
+let age = 34
+let isLogged = false;
+console.table([name, age, isLogged])
+console.log(typeof "123")
+console.log(typeof null)
