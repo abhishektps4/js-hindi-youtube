6e38e1438695dffd4b1c58b49849cf2530595c62
abhishektps4 <abhishek1720044@gmail.com>
2024-12-08 17:38:43 +0000

    conversionOperation

---
+++ 03_conversionOperation.js
@@ -0,0 +1,21 @@
+let value   =4;
+let negVlaue = -value
+//console.log(negVlaue)
+// console.log(2**3)
+// console.log(2%4)
+
+let str1 = "hello"
+let str2 = " abhishek"
+let str3 = str1 +str2
+//console.log(str3)
+
+// console.log(1+"4") //14
+// console.log("1"+4)  //14 
+// console.log("1" + "4")  // 14
+// console.log("1" + 4 +4)  // 144
+// console.log(1 + 4 + "4")  // 54
+
+
+// console.log(+true) //1
+// console.log(+"") // 0
+
