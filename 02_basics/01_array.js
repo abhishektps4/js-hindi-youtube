ab8ca07d05f7c204968c537a0e8594b8b501a01e
abhishektps4 <abhishek1720044@gmail.com>
2024-12-09 13:25:27 +0000

    Array1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,30 @@
+const arr = [0,1,2,3,4,5, true , "abhishek"]
+// console.log(arr[3]) 
+// arr.push(6)
+// console.log(arr) 
+// arr.pop()
+// console.log(arr) 
+// arr.unshift(8)
+// console.log(arr) 
+// arr.shift()
+// console.log(arr) 
+
+// console.log(arr.includes(9))
+// console.log(arr.indexOf(4))
+// const newArr = arr.join()
+// console.log(arr)
+// console.log(newArr)
+// console.log(typeof newArr)
+
+// slice splice
+console.log(arr)
+const nArr =  arr.slice(1,3)
+console.log(nArr)
+console.log("A" , nArr)
+const nArr2 =  arr.splice(1,3)
+console.log("C", nArr)
+console.log( "B" ,nArr2)
+console.log(arr)
+
+
+
