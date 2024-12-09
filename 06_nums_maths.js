80574bcfdd2af5378b3d9fdfcfb858576381c51d
abhishektps4 <abhishek1720044@gmail.com>
2024-12-09 11:08:26 +0000

    maths and nums

---
+++ 06_nums_maths.js
@@ -0,0 +1,29 @@
+const score  = 100;
+//console.log(score)
+const balance = new Number(234.564878)
+//console.log(balance)
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(2))
+const otherNumber = 1523.85585555555
+
+//console.log(otherNumber.toPrecision(3))
+const hundreds = 10000000;
+//console.log(hundreds.toLocaleString('en-IN'))
+
+//+++++++++++++++maths++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5)) //5
+// console.log(Math.ceil(4.3)) //5
+// console.log(Math.floor(4.7)) //4
+// console.log(Math.min(3 ,4,3,7,44,6,2))
+// console.log(Math.max(4,5,7,4,7,9))
+console.log(Math.random()) // o<x<1 range
+console.log(Math.floor(Math.random()*10)+1)
+
+const min =10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1)) + min )
+
+
