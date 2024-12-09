128a5b54f070b6d11c93de7411ea2fa25e575a75
abhishektps4 <abhishek1720044@gmail.com>
2024-12-09 10:37:32 +0000

    strings

---
+++ 05_strings.js
@@ -0,0 +1,26 @@
+const name = "Abhishek Raj"
+const Repocount = 50;
+console.log(name + Repocount + "gty");
+console.log(`Hello my name is ${name} and my repo ciunt is ${Repocount}`);
+console.log(`Hello my name is ${name} and my repo ciunt is ${Repocount}`);
+const gameName = new String('abhishekr')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('p'));
+const newString = gameName.substring(0,4);
+console.log(newString);
+anotherString  = gameName.slice(-8,6);
+console.log(anotherString);
+const ert = "  jht";
+console.log(ert);
+console.log(ert.trim())
+
+const url = "http://hitesh.com"
+console.log(url.replace('te' , '_'))
+console.log(url.includes('hi'))
+console.log(url.includes('io'))
+const yt = "abhishek-raj-put"
+console.log(yt.split('-'))
