1dc9fa4949a0a50fd0044087a06aa0c1aec13a3b
abhishektps4 <abhishek1720044@gmail.com>
2024-12-10 07:38:39 +0000

    Destructuring

---
+++ 02_basics/destructuring.js
@@ -0,0 +1,24 @@
+const  course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor:"hitesh"
+}
+
+//course.courseInstructor
+const {courseInstructor: instructor} = course
+//console.log(courseInstructor)
+console.log(instructor)
+
+//JSON / API
+// {
+//     "name": "Abhishek",
+//     "class" : "Undergraduate",
+//     "Age" : 20
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
