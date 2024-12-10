73a9466f461a04cd920ce35e4ad84cb909dc923d
abhishektps4 <abhishek1720044@gmail.com>
2024-12-10 13:27:51 +0000

    one control flow

---
+++ control_flow/one.js
@@ -0,0 +1,23 @@
+// if statement '
+
+if(2=="2"){
+    console.log("executed")
+}
+
+const temperature = 41;
+if(temperature === 41){
+    console.log("Abhishek Raj")
+
+}
+
+console.log("hello")
+
+const score = 300
+if(score >100){
+    const power = "fly"
+    console.log(`Use power : ${power}`)
+}
+// console.log(`Use power : ${power}`)  wrong 
+
+//bad practice if(balance > 400) console.log("test") , console.log("test3")
+
