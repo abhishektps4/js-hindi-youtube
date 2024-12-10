97b5389cc4138678a538485a5dd1f1ca7e7a6457
abhishektps4 <abhishek1720044@gmail.com>
2024-12-10 13:37:30 +0000

    switch case

---
+++ control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch(key) {
+//     case value: 
+//                break;
+
+//           default : 
+//                    break;     
+// }
+
+
+const month     = 4;
+switch(4){
+    case 1:
+         console.log("Jan");
+         break;
+    case 2:
+         console.log("Feb");
+         break;
+    case 3:
+         console.log("March");
+         break;
+    case 4:
+         console.log("April");
+          break;
+
+    default :
+            console.log("Invalid");
+            break;   
+        
+}
